d2b5085d121bab863966d01928dabc99c8ae591b
menglanmei6 <1229805664@qq.com>
2019-03-27 09:19:57 +0800

    创建托管Fragment(3.27 9:19)

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -3,8 +3,10 @@ package net.lzzy.cinemanager.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
@@ -13,16 +15,20 @@ import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.FragmentAddCinemas;
+import net.lzzy.cinemanager.fragments.FragmentAddOrders;
 import net.lzzy.cinemanager.fragments.OrdersFragment;
 
 /**
  * @author Administrator
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private FragmentManager manager=getSupportFragmentManager();
+    private FragmentManager manager = getSupportFragmentManager();
     private View layoutMenu;
     private SearchView search;
     private TextView tvTitle;
+    private SparseArray<String> titleArray = new SparseArray<>();
+    private SparseArray<Fragment> fragmentArray = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         setTitleMenu();
     }
-    private void setTitleMenu(){
+
+    private void setTitleMenu() {
+        titleArray.put(R.id.bar_title_tv_add_cinema, "添加影院");
+        titleArray.put(R.id.bar_title_tv_view_cinema, "影院列表");
+        titleArray.put(R.id.bar_title_tv_add_order, "添加订单");
+        titleArray.put(R.id.bar_title_tv_view_order, "我的订单");
         layoutMenu = findViewById(R.id.bar_title_layout_menu);
         layoutMenu.setVisibility(View.GONE);
         findViewById(R.id.bar_title_img_menu).setOnClickListener(v -> {
-            int visible=layoutMenu.getVisibility()== View.VISIBLE?View.GONE:View.VISIBLE;
+            int visible = layoutMenu.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE;
             layoutMenu.setVisibility(visible);
         });
         tvTitle = findViewById(R.id.bar_title_tv_title);
@@ -51,27 +62,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         layoutMenu.setVisibility(View.GONE);
-      switch (v.getId()){
-          case R.id.bar_title_tv_add_cinema:
-              break;
-          case R.id.bar_title_tv_view_cinema:
-              tvTitle.setText("影院列表");
-              manager.beginTransaction()
-                      .replace(R.id.fragment_container,new CinemasFragment())
-                      .commit();
-              break;
-          case R.id.bar_title_tv_add_order:
-              break;
-          case R.id.bar_title_tv_view_order:
-              tvTitle.setText("我的订单");
-              manager.beginTransaction()
-                      .replace(R.id.fragment_container,new OrdersFragment())
-                      .commit();
-              break;
-              default:
-                  break;
-      }
-
+        tvTitle.setText(titleArray.get(v.getId()));
+        FragmentTransaction transaction = manager.beginTransaction();
+        Fragment fragment = fragmentArray.get(v.getId());
+        if (fragment == null) {
+            fragment = createFragment(v.getId());
+            fragmentArray.put(v.getId(), fragment);
+            transaction.add(R.id.fragment_container, fragment);
+        }
+        for (Fragment f : manager.getFragments()) {
+            transaction.hide(f);
+        }
+        transaction.show(fragment).commit();
     }
+
+
+    private Fragment createFragment(int id) {
+        switch (id) {
+            case R.id.bar_title_tv_add_cinema:
+               return new FragmentAddCinemas();
+            case R.id.bar_title_tv_view_cinema:
+                return new CinemasFragment();
+            case R.id.bar_title_tv_add_order:
+               return new FragmentAddOrders();
+
+            case R.id.bar_title_tv_view_order:
+                return new OrdersFragment();
+            default:
+                break;
+        }
+        return null;
+    }
+
 }
 
---
+++ app/src/main/java/net/lzzy/cinemanager/fragments/FragmentAddCinemas.java
@@ -0,0 +1,32 @@
+package net.lzzy.cinemanager.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import net.lzzy.cinemanager.R;
+
+/**
+ * Created by lzzy_gxy on 2019/3/27.
+ * Description:
+ */
+public class FragmentAddCinemas extends Fragment {
+    public FragmentAddCinemas(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_add_cinemas,null);
+        TextView tv=view.findViewById(R.id.fragment_add_cinemas_edt);
+        return view;
+    }
+
+}
